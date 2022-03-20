4dc88b73014256c3fce9ff5fd3b18f97294b155f
akimajid <akimajid07@gmail.com>
2022-03-20 20:31:30 +0700

    ref: setup redux

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -3,16 +3,21 @@ import { ChakraProvider } from "@chakra-ui/react";
 import { Provider } from "react-redux";
 import Navbar from "../component/Navbar";
 import Footer from "../component/Footer";
+import rootReducer from "../redux/store";
+import thunk from "redux-thunk";
+import { createStore, applyMiddleware } from "redux";
+
+const store = createStore(rootReducer, applyMiddleware(thunk));
 
 function MyApp({ Component, pageProps }) {
   return (
-    // <Provider>
-    <ChakraProvider>
-      <Navbar />
-      <Component {...pageProps} />
-      <Footer />
-    </ChakraProvider>
-    // </Provider>
+    <Provider store={store}>
+      <ChakraProvider>
+        <Navbar />
+        <Component {...pageProps} />
+        <Footer />
+      </ChakraProvider>
+    </Provider>
   );
 }
 
